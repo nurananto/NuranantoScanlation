560e692412f039ad2f5c517fc25b014b1abb0bdd
nurananto <34748019+nurananto@users.noreply.github.com>
2025-10-09 16:45:31 +0700

    Update

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 // CARA PAKAI:
-// Manga yang BARU DIUPDATE → Taruh di PALING ATAS array ini
+// Manga yang BARU DIUPDATE â†’ Taruh di PALING ATAS array ini
 // Urutan dari atas ke bawah = urutan tampil di website
 
 const mangaList = [
+  {
+    title: "Yarikonda Renai Game no Akuyaku ni Tensei shitanode",
+    cover: "covers/yarikonda.jpg",
+    readFirst: "https://trakteer.id/NuranantoScanlation",
+    readNow: "https://cubari.moe/proxy/gist/cmF3L251cmFuYW50by9OdXJhbmFudG9DdWJhcmlNb2UvbWFpbi9ZYXJpa29uZGElMjBSZW5haSUyMEdhbWUlMjBubyUyMEFrdXlha3UlMjBuaSUyMFRlbnNlaSUyMHNoaXRhbm9kZSwlMjBHZW5zYWt1JTIwQ2hpc2hpa2klMjBkZSUyMEhlcm9pbmUlMjB3byUyMEtvdXJ5YWt1JTIwc2hpbWFzdS5qc29u/",
+    mangadex: "https://mangadex.org/title/a605a5d0-21a6-481f-a055-74735ea4f2c2/yarikonda-renai-game-no-akuyaku-ni-tensei-shitanode-gensaku-chishiki-de-heroine-wo-kouryaku-shimasu"
+  },
   {
     title: "Yuumei VTuber no Ani Dakedo, Nazeka Ore ga Yuumei ni Natteita",
     cover: "covers/vtuber.jpg",
@@ -30,13 +37,6 @@ const mangaList = [
     readFirst: "https://trakteer.id/NuranantoScanlation",
     readNow: "https://cubari.moe/proxy/gist/cmF3L251cmFuYW50by9OdXJhbmFudG9DdWJhcmlNb2UvbWFpbi9VY2hpJTIwbm8lMjBTZWlzby1rZWklMjBJaW5jaG91JTIwZ2ElMjBLYXRzdXRlJTIwQ2h1dW5pYnlvdSUyMElkb2wlMjBkYXR0YSUyMEtvdG8lMjBvJTIwT3JlJTIwRGFrZSUyMGdhJTIwU2hpdHRlaXJ1Lmpzb24/",
     mangadex: "https://mangadex.org/title/5993c10b-c49e-4771-9a3a-8b8436b12d80/uchi-no-seiso-kei-iinchou-ga-katsute-chuunibyou-idol-datta-koto-o-ore-dake-ga-shitteiru"
-  },
-  {
-    title: "Yarikonda Renai Game no Akuyaku ni Tensei shitanode",
-    cover: "covers/yarikonda.jpg",
-    readFirst: "https://trakteer.id/NuranantoScanlation",
-    readNow: "https://cubari.moe/proxy/gist/cmF3L251cmFuYW50by9OdXJhbmFudG9DdWJhcmlNb2UvbWFpbi9ZYXJpa29uZGElMjBSZW5haSUyMEdhbWUlMjBubyUyMEFrdXlha3UlMjBuaSUyMFRlbnNlaSUyMHNoaXRhbm9kZSwlMjBHZW5zYWt1JTIwQ2hpc2hpa2klMjBkZSUyMEhlcm9pbmUlMjB3byUyMEtvdXJ5YWt1JTIwc2hpbWFzdS5qc29u/",
-    mangadex: "https://mangadex.org/title/a605a5d0-21a6-481f-a055-74735ea4f2c2/yarikonda-renai-game-no-akuyaku-ni-tensei-shitanode-gensaku-chishiki-de-heroine-wo-kouryaku-shimasu"
   },
   {
     title: "Kimi no Negai ga Kanau made",
